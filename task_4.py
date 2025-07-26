539aa39fc6a6b563ef3e78a7e5d55593e2500c43
andrey-kv13 <andreyfbinst@gmail.com>
2025-07-26 21:06:36 +0300

    Fix @classmethod  (PR comments)

--- task_4.py
+++ task_4.py
@@ -1,26 +1,27 @@
 class EmployeeSalary:
     
-    def __init__(self, name, hours, rest_days, email, hourly_payment = 400,):
+    def __init__(self, name, hours, rest_days, email, hourly_payment=400):
         self.name = name
         self.hours = hours
         self.rest_days = rest_days
         self.email = email
         self.hourly_payment = hourly_payment
         
+    def get_hours(self):
+        if self.hours is None:
+           self.hours = (7 - self.rest_days) * 8
+        return self.hours
+
+    def get_email(self):
+        if self.email is None:
+           self.email = f"{self.name}@email.com"
+        return self.email
+    
     @classmethod
-    def get_hours(cls, rest_days):
-        hours = (7 - rest_days) * 8
-        return hours
-        
-    @classmethod
-    def get_email(cls, name):
-        email = f"{name}@email.com"
-        return email
-        
-    def set_hourly_payment(self,new_hourly_payment):
-        self.hourly_payment = new_hourly_payment
+    def set_hourly_payment(cls, new_hourly_payment):
+        cls.hourly_payment = new_hourly_payment
     
     def salary(self):
-        hours = self.get_hours(self.rest_days)
+        hours = self.get_hours()
         salary = hours * self.hourly_payment
         return salary
